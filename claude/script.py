f279ec78e1180bd4322bb3db2144bfb48a80303d
Samuel Armstrong <samuelarmstrong@Samuels-MacBook-Air.local>
2023-11-04 23:35:50 +0000

    first part of merging the docstrings into original file + bug fixes

--- claude/script.py
+++ claude/script.py
@@ -2,6 +2,10 @@ from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 import sys
 import json
 import os
+import fileinput
+
+
+filename = "dummy-files/test.py"
 
 
 def generate_docstring(file_str, fn_name, key):
@@ -32,8 +36,7 @@ def generate_docstring(file_str, fn_name, key):
 
 def starter_code(key):
     # diff text file all strings, parse the file to only fetch statements with additions
-       # changed file names
-    filename = "dummy-files/test.py"
+    # changed file names
     with open("diff.txt", '+rb') as f:
         # intelligent regex 
         content = f.readlines()
@@ -46,7 +49,7 @@ def starter_code(key):
                 in_func = True
                 func_name = line.split('+def ')[1].split('(')[0]
                 # regex to check if there exists a docstring
-            if line.replace(' ', '') == '+\n':
+            if line.replace(' ', '') == '+\n' or line == '\n':
                 if in_func and not contains_docstring:
                     fns_without_docstring[func_name] = generate_docstring(filename, func_name, key)
                 in_func = False
@@ -55,9 +58,40 @@ def starter_code(key):
             if '"""' in line:
                 contains_docstring = True
     return fns_without_docstring
+
+
+def merge_docstring(fns_without_docstring):
+    with open(filename, '+rb') as f:
+        content = f.readlines()
+        fn_wo_doc = False
+        current_docstring = ""
+        docstring_placement = {}
+        for i, line in enumerate(content):
+            line = line.decode('utf-8')
+            if any([fn_name in line for fn_name in fns_without_docstring.keys()]) and "def" in line:  # if a fn without docstring is defined on this line
+                fn_wo_doc = True
+                for fn_name in fns_without_docstring.keys():
+                    if fn_name in line:
+                        current_docstring = fns_without_docstring[fn_name]
+                        break
+            if ":" in line and fn_wo_doc:
+                docstring_placement[i] = current_docstring
+                current_docstring = ""
+                fn_wo_doc = False
     
+    print('docstring_placement', docstring_placement)
+
+    # with fileinput.input(files=(filename,), inplace=True) as file:
+    #     for line_num, line in enumerate(file, start=1):
+    #         # Check if this line should have content added
+    #         if line_num in fns_to_add:
+    #             content_to_add = fns_to_add[line_num]
+    #             print(content_to_add, end='')
+    #         print(line, end='')
+
 if __name__ == "__main__":
     key = sys.argv[1]
     docstring_dict = starter_code(key)
+    merge_docstring(docstring_dict)
     print(docstring_dict)
     
