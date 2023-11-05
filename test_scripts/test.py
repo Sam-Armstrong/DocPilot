a138a853465e6de7af943c74bbf9f0bb4c8d20ca
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2023-11-05 09:21:54 +0000

    add examples section to docstring template

--- test_scripts/test.py
+++ test_scripts/test.py
@@ -11,6 +11,7 @@ import ivy
 from ivy.func_wrapper import with_unsupported_dtypes, with_supported_dtypes
 from . import backend_version
 from ivy.functional.ivy.layers import _get_embed_dim, _handle_padding, _deconv_length
+import tensorflow as tf
 
 
 @with_supported_dtypes(
@@ -1520,3 +1521,73 @@ def as_ivy_dtype(
         raise ivy.utils.exceptions.IvyException(
             f"Cannot recognize {dtype_str} as a valid Dtype."
         )
+
+
+def concat(
+    xs: Union[Tuple[tf.Tensor, ...], List[tf.Tensor]],
+    /,
+    *,
+    axis: int = 0,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+) -> Union[tf.Tensor, tf.Variable]:
+    is_tuple = type(xs) is tuple
+    is_axis_none = axis is None
+    if is_tuple:
+        xs = list(xs)
+    highest_dtype = xs[0].dtype
+    for i in xs:
+        highest_dtype = ivy.as_native_dtype(ivy.promote_types(highest_dtype, i.dtype))
+
+    for i in range(len(xs)):
+        if is_axis_none:
+            xs[i] = tf.reshape(xs[i], -1)
+        xs[i] = ivy.astype(xs[i], highest_dtype, copy=False).to_native()
+    if is_axis_none:
+        axis = 0
+        if is_tuple:
+            xs = tuple(xs)
+    try:
+        return tf.concat(xs, axis)
+    except (tf.errors.InvalidArgumentError, np.AxisError) as error:
+        raise ivy.utils.exceptions.IvyIndexError(error)
+
+
+def expand_dims(
+    x: Union[tf.Tensor, tf.Variable],
+    /,
+    *,
+    copy: Optional[bool] = None,
+    axis: Union[int, Sequence[int]] = 0,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+) -> Union[tf.Tensor, tf.Variable]:
+    try:
+        out_shape = _calculate_out_shape(axis, x.shape)
+        ret = tf.reshape(x, shape=out_shape)
+        return ret
+    except (tf.errors.InvalidArgumentError, np.AxisError) as error:
+        raise ivy.utils.exceptions.IvyIndexError(error)
+
+
+def flip(
+    x: Union[tf.Tensor, tf.Variable],
+    /,
+    *,
+    copy: Optional[bool] = None,
+    axis: Optional[Union[int, Sequence[int]]] = None,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+) -> Union[tf.Tensor, tf.Variable]:
+    num_dims = len(x.shape)
+    if not num_dims:
+        ret = x
+    else:
+        if axis is None:
+            new_axis = list(range(num_dims))
+        else:
+            new_axis = axis
+        if type(new_axis) is int:
+            new_axis = [new_axis]
+        else:
+            new_axis = new_axis
+        new_axis = [item + num_dims if item < 0 else item for item in new_axis]
+        ret = tf.reverse(x, new_axis)
+    return ret
