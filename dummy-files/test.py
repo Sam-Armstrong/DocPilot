bd2e5adfa8cbef60fb7816682f7c85d98155b18b
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2023-11-05 02:45:07 +0000

    add docstring

--- dummy-files/test.py
+++ dummy-files/test.py
@@ -16,6 +16,35 @@ from . import backend_version
 
 
 def _reshape_fortran_tf(x, shape):
+    """
+    Reshapes the input array x into the shape provided in the shape argument. 
+    
+    Parameters
+    ----------
+    x : tf.Tensor or tf.Variable
+        Input tensor to be reshaped.
+    shape : Sequence[int]
+        The new shape of the input array.
+    copy : bool, optional
+        Default is None. Whether to copy the input array or operate in-place.
+    order : {'C', 'F'}, optional
+        Default is 'C'. Whether to reshape in C-order (row-major) or Fortran-order (column-major).
+    allowzero : bool, optional
+        Default is True. Whether to allow one of the shape dimensions to be zero. 
+        If False, a zero dimension raises an error.
+    out : tf.Tensor or tf.Variable, optional 
+        Default is None. Output tensor.
+    
+    Returns
+    -------
+    ret : tf.Tensor or tf.Variable
+        Reshaped input tensor.
+    
+    Raises
+    ------
+    ivy.exceptions.IvyIndexError
+        If input shape contains a zero dimension when allowzero=False.
+    """
     if len(x.shape) > 0:
         x = tf.transpose(x)
     return tf.transpose(tf.reshape(x, shape[::-1]))
@@ -57,6 +86,35 @@ def reshape(
     allowzero: bool = True,
     out: Optional[Union[tf.Tensor, tf.Variable]] = None,
 ) -> Union[tf.Tensor, tf.Variable]:
+    """
+    Reshapes the input array x into the shape provided in the shape argument. 
+    
+    Parameters
+    ----------
+    x : tf.Tensor or tf.Variable
+        Input tensor to be reshaped.
+    shape : Sequence[int]
+        The new shape of the input array.
+    copy : bool, optional
+        Default is None. Whether to copy the input array or operate in-place.
+    order : {'C', 'F'}, optional
+        Default is 'C'. Whether to reshape in C-order (row-major) or Fortran-order (column-major).
+    allowzero : bool, optional
+        Default is True. Whether to allow one of the shape dimensions to be zero. 
+        If False, a zero dimension raises an error.
+    out : tf.Tensor or tf.Variable, optional 
+        Default is None. Output tensor.
+    
+    Returns
+    -------
+    ret : tf.Tensor or tf.Variable
+        Reshaped input tensor.
+    
+    Raises
+    ------
+    ivy.exceptions.IvyIndexError
+        If input shape contains a zero dimension when allowzero=False.
+    """
     ivy.utils.assertions.check_elem_in_list(order, ["C", "F"])
     if not allowzero:
         shape = [
