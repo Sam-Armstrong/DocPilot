8a2e40a136906f358d3ef62ca2b8f7358bda044c
Aarsh2001 <ac5973@srmist.edu.in>
2023-11-05 00:40:07 +0000

    re-engineer the test examples

--- dummy-files/test.py
+++ dummy-files/test.py
@@ -65,3 +65,60 @@ def random_normal(
         tf.random.set_seed(seed)
     return tf.random.normal(shape, mean, std, dtype=dtype, seed=seed)
 
+
+@with_unsupported_dtypes({"2.13.0 and below": ("bfloat16",)}, backend_version)
+def multinomial(
+    population_size: int,
+    num_samples: int,
+    /,
+    *,
+    batch_size: int = 1,
+    probs: Optional[Union[tf.Tensor, tf.Variable]] = None,
+    replace: bool = True,
+    device: str = None,
+    seed: Optional[int] = None,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+) -> Union[tf.Tensor, tf.Variable]:
+    if probs is None:
+        probs = (
+            tf.ones(
+                (
+                    batch_size,
+                    population_size,
+                )
+            )
+            / population_size
+        )
+
+    # We set the global seed, but not the operation seeds below. In this way, we
+    # get different results for every random op call but the same sequence for
+    # every re-run of the program
+    if seed:
+        tf.random.set_seed(seed)
+
+    if not replace:
+        orig_probs_shape = list(probs.shape)
+        probs_flat = tf.reshape(probs, (-1, orig_probs_shape[-1]))
+        probs_flat = probs_flat / tf.math.reduce_sum(probs_flat, axis=-1, keepdims=True)
+        probs_stack = tf.split(probs_flat, probs_flat.shape[0])
+        samples_stack = []
+        for prob in probs_stack:
+            logits = tf.dtypes.cast(tf.math.log(prob), tf.float64)
+            # Gumbel-max trick
+            # https://github.com/tensorflow/tensorflow/issues/9260
+            z = tf.dtypes.cast(
+                -tf.math.log(-tf.math.log(tf.random.uniform(tf.shape(logits), 0, 1))),
+                tf.float64,
+            )
+            _, indices = tf.nn.top_k(logits + z, k=num_samples)
+            samples_stack.append(indices)
+        samples_flat = tf.stack(samples_stack)
+        return tf.convert_to_tensor(
+            tf.reshape(samples_flat, orig_probs_shape[:-1] + [num_samples])
+        )
+    else:
+        if len(probs.numpy().shape) == 1:
+            probs = tf.expand_dims(probs, axis=0)
+        return tf.random.categorical(tf.math.log(probs), num_samples)
+
+
